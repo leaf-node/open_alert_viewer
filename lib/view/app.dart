38d080e8252c02b66ff3d1e2bd54399ce9119ee2
Andrew Engelbrecht <andrew@sourceflow.dev>
2024-05-13 12:28:53 -0400

    Don't show debug banner

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: AlertsPage(title: 'Open Alert Viewer', sources: [RandomAlerts()]),
     );
   }
