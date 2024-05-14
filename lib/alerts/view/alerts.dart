3fd56c9be8ef48973e475216f5851442daefb22d
Andrew Engelbrecht <andrew@sourceflow.dev>
2024-05-14 14:03:40 -0400

    Indicator when scrolling to refresh

--- lib/alerts/view/alerts.dart
+++ lib/alerts/view/alerts.dart
@@ -72,12 +72,19 @@ class AlertsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AlertsBloc, AlertState>(builder: (context, state) {
+    return RefreshIndicator(onRefresh: () async {
+      context
+          .read<AlertsBloc>()
+          .add(const FetchAlerts(maxCacheAge: Duration.zero));
+      await context.read<AlertsBloc>().stream.firstWhere(
+            (state) => state is! AlertsFetching,
+          );
+    }, child: BlocBuilder<AlertsBloc, AlertState>(builder: (context, state) {
       List<Widget> alertWidgets = [];
       for (var alert in state.alerts) {
         alertWidgets.add(AlertWidget(alert: alert));
       }
       return ListView(children: alertWidgets);
-    });
+    }));
   }
 }
