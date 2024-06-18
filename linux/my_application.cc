79b3eee1deb955603463deec6fdca986eb5dc07d
Andrew Engelbrecht <andrew@sourceflow.dev>
2024-06-17 20:13:34 -0400

    Fix name of application in GTK header bar

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -46,11 +46,11 @@ static void my_application_activate(GApplication* application) {
     if (use_header_bar) {
       GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
       gtk_widget_show(GTK_WIDGET(header_bar));
-      gtk_header_bar_set_title(header_bar, "open_alert_viewer");
+      gtk_header_bar_set_title(header_bar, "Open Alert Viewer");
       gtk_header_bar_set_show_close_button(header_bar, TRUE);
       gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
     } else {
-      gtk_window_set_title(window, "open_alert_viewer");
+      gtk_window_set_title(window, "Open Alert Viewer");
     }
   
     gtk_window_set_default_size(window, 1280, 720);
