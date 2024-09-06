86c2744ce2ce14d06273ea85b6e157379c45fd68
Andrew Engelbrecht <andrew@okaycode.dev>
2024-09-06 16:52:00 -0400

    Add git short commit ID to version string

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,22 @@ Future<void> startApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   var db = LocalDatabase();
   await db.migrate(showPath: true);
-  String appVersion =
-      loadYaml(await rootBundle.loadString("pubspec.yaml"))["version"];
   var bgWorker = BackgroundWorker();
+  String appVersion = await getVersion();
   await bgWorker.spawn(appVersion: appVersion);
   runApp(OAVapp(appVersion: appVersion, db: db, bgWorker: bgWorker));
+}
+
+Future<String> getVersion() async {
+  String gitVersion;
+  final pubspecVersion =
+      loadYaml(await rootBundle.loadString("pubspec.yaml"))["version"];
+  var head = await rootBundle.loadString('.git/HEAD');
+  if (head.startsWith('ref: ')) {
+    final branchName = head.split('ref: refs/heads/').last.trim();
+    gitVersion = await rootBundle.loadString('.git/refs/heads/$branchName');
+  } else {
+    gitVersion = head;
+  }
+  return "$pubspecVersion-${gitVersion.substring(0, 8)}";
 }
